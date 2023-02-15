d779ee98c8864af1e63a604260f2a67c967a85ee
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-14 23:04:48 -0300

    Created Request Class
    Created Request Class with def init, request, print_result.

--- ClassRequest.py
+++ ClassRequest.py
@@ -1,7 +1,28 @@
-import requests
+import pip._vendor.requests 
 
-class request:
-  def __init__(self):
-    self.request = None
+class Request:
+    def __init__(self):
+        self.cep = self.inputcep()
+        self.link = f'https://viacep.com.br/ws/{self.cep}/json/'
+        self.response = self.request()
+        self.print_result = self.print_result()
+        
+    def inputcep(self):
+        cep = int(input('Digite seu CEP: '))
+        return cep
     
+    def request(self):
+        response = pip._vendor.requests.get(self.link)
+        dic_response = response.json()
+        self.state = dic_response['uf']
+        self.city = dic_response['localidade']
+        self.road = dic_response['logradouro']
+        self.nh = dic_response['bairro']
+        self.address = (self.state, self.city, self.road, self.nh)
+        return self.address
+            
+    def print_result(self):
+        print(self.address)
+        return
+
     
