3ee29c7d9fbd2f08eb09f6ecb947a189bc22ca13
Lilian Matsumoto <109561962+liliansom@users.noreply.github.com>
2023-02-26 01:03:49 -0300

    Inseridos Comentários

--- ClassRequest.py
+++ ClassRequest.py
@@ -1,16 +1,20 @@
 import pip._vendor.requests
 
+
+# Class for the consume of ZipCode API
 class Request:
     def __init__(self):
         self.cep = self.inputcep()
         self.link = f'https://viacep.com.br/ws/{self.cep}/json/'
         self.response = self.request()
         self.print_result = self.print_result()
-        
+     
+    # Method to enter the ZipCode
     def inputcep(self):
         cep = int(input('Digite seu CEP: '))
         return cep
     
+    # Method for requesting data from the API
     def request(self):
         response = pip._vendor.requests.get(self.link)
         dic_response = response.json()
@@ -21,6 +25,7 @@ class Request:
         self.address = (self.road, self.nh, self.city, self.state)
         return self.address
     
+    # Method for presenting the requested data
     def print_result(self):
         print(self.address)
         return
